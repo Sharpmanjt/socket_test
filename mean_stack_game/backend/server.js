6b9c009cf64181d4ebf74f09d2cf38aef2af255d
sharpmanjt <thibeau.jeremy@gmail.com>
2020-03-04 11:46:44 -0500

    adding methods for gameplay

--- mean_stack_game/backend/server.js
+++ mean_stack_game/backend/server.js
@@ -41,8 +41,8 @@ app.use(function (err, req, res, next) {
 var io = require("socket.io")(server);
 
 var position = {
-    x: 200,
-    y: 200
+    x: 230,
+    y: 400
 };
 
 io.on('connection', (socket) => {
