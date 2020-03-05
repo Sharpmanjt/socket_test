4b2f571867b969db91c83b902e88ed697698ced1
Veronyque Lemieux <veronyque.lemieux@gmail.com>
2020-03-04 21:46:31 -0500

    added routing to button

--- mean_stack_game/src/app/app-routing.module.ts
+++ mean_stack_game/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GameComponent } from '../app/game/game.component';
+import { MenuComponent } from './menu/menu.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/menu', pathMatch: 'full'},
+  { path: 'playgame', component: GameComponent },
+  { path: 'menu', component: MenuComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
