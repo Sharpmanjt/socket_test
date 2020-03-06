6e0aea4dedf164f1fee55dd0a2e30c1c643c16ce
sergiolombana <sergiolombana101@gmail.com>
2020-03-06 15:01:01 -0500

    Removing enemy on shot

--- mean_stack_game/src/app/game/game.component.ts
+++ mean_stack_game/src/app/game/game.component.ts
@@ -25,7 +25,7 @@ export class GameComponent implements OnInit {
     
     @HostListener('document:keydown', ['$event'])
     handleKeyboardEvent(event: KeyboardEvent) { 
-        event.preventDefault();
+        //event.preventDefault();
         console.log(event.key);
         var keypress = this.readKey(event.key)
         if(keypress != 'empty') this.move(keypress)
@@ -64,9 +64,9 @@ startTimer() {
     public ngAfterViewInit() {
       this.context = this.gameCanvas.nativeElement.getContext("2d");
       this.socket.on("position", data => {
-          console.log(data.oldposx)
-          console.log(data.oldposy)
-          console.log(data.position)
+          //console.log(data.oldposx)
+          //console.log(data.oldposy)
+          //console.log(data.position)
           //this.context.clearRect(0, 0, this.gameCanvas.nativeElement.width, this.gameCanvas.nativeElement.height);
           this.context.clearRect(data.oldposx, data.oldposy, 35, 40);
           let space_img = document.createElement("img");
@@ -75,26 +75,8 @@ startTimer() {
           this.context.drawImage(space_img, data.position.x, data.position.y, 35, 40)
 
         
-          /*space_img.onload = function(){
-          this.context.drawImage(space_img, 230, 400, 35, 40);
-      });*/
     })
-      /*this.socket.on("shoot", data =>{
-        var shot = {'x': data.x, 'y': data.y-5}
-        var ctx = this.context;
-        var canvas = this.gameCanvas
-        for (var x = 0; x < 10; x++)
-        {
-            setTimeout(function(){
-            ctx.clearRect(0, 0, canvas.nativeElement.width, canvas.nativeElement.height);
-            ctx.fillStyle ='blue'
-            ctx.fillRect(shot['x'], shot['y'], 10, 10)
-            shot['y'] -= 5
-            ctx.fillStyle = 'black'
-            ctx.fillRect(data.x, data.y, 20, 20);
-            }, 2000);
-        }        
-    })*/
+
 
     this.socket.on("shoot", data=>{
         let laser = this.createLaserElement(data.x,data.y); 
@@ -120,42 +102,77 @@ startTimer() {
     let x_position = x
     let y_position = y
     let laserInterval = setInterval(()=>{
+        console.log("Moving: "+x_position+", "+y_position);
         this.context.clearRect(x_position,y_position, 35, 40);
-        if(y_position < -50){ //THE LIMIT OF THE SCREEN
+        if(y_position < 5){ //THE LIMIT OF THE SCREEN
             this.context.clearRect(x_position,y_position, 35, 40);
+            clearInterval(laserInterval);
         }else{
             y_position -= 5
             this.context.drawImage(laser,x_position,y_position,35,40);
         }
         this.move("appear"); //IT CALLS THE SERVER TO POSITION THE SPACESHIP AT THE LAST POSITION RECORDED
-        //this.checkIfEnemyWasShot(x_position, y_position);
+        //console.log("This bullet x: "+x_position+". This bullet y:"+y_position);
+        let index = this.checkIfEnemyWasShot(x_position, y_position);
+        if(index != -1){
+            clearInterval(laserInterval);
+            this.destroyEnemy(index);
+        }
+        /*if(index != -1){
+            let context = this.context;
+            console.log("About to destroy enemy");
+            this.destroyEnemy(index);
+            let remove_explosion = setTimeout(()=>{
+                let explosion_x = this.Enemies_1[index].position_x;
+                let explosion_y = this.Enemies_1[index].position_y;
+                context.clearReact(explosion_x,explosion_y,35,40);
+            },2000)
+            remove_explosion;
+            clearInterval(laserInterval);
+        }*/
     },10)
+    for(let enemy in this.Enemies_1){
+        let index = this.Enemies_1.indexOf(this.Enemies_1[enemy]);
+        console.log(index+" : "+JSON.stringify(this.Enemies_1[enemy]));
+    }
   }
 
   public checkIfEnemyWasShot(x,y){
     for(let enemy in this.Enemies_1){
-        if((this.Enemies_1[enemy].position_x >= x+4 && this.Enemies_1[enemy].position_x <= x-4)
+        if((this.Enemies_1[enemy].position_x + 10 >= x && this.Enemies_1[enemy].position_x - 10 <= x)
             &&
-           (this.Enemies_1[enemy].position_y >= y+4 && this.Enemies_1[enemy].position_y <= y-4)){
-                let index = this.Enemies_1.indexOf(this.Enemies_1[enemy]);
-                console.log("Enemy called at: "+index);
-            }
-        //console.log("X:"+this.Enemies_1[enemy].position_x+" Y:"+this.Enemies_1[enemy].position_y)
+           (this.Enemies_1[enemy].position_y >= y && this.Enemies_1[enemy].position_y - 5 <= y)){
+            console.log("Enemy shot!");
+            let index = this.Enemies_1.indexOf(this.Enemies_1[enemy]);
+            return index-1;
+           }
     }
+    return -1;
   }
 
-  public destroyEnemy(//player1, player2
-  ){
-    /*player.score += 10
-    //if player1.score >= 500{
-        winner = player1                                          
+  public destroyEnemy(index){
+    let Enemy_posX = this.Enemies_1[index].position_x;
+    let Enemey_posY = this.Enemies_1[index].position_y;
+       
+    this.context.clearRect(Enemy_posX, Enemey_posY, 35, 40); //REMOVES ENEMY FROM CANVAS
+    let explosion_img = document.createElement("img");
+    explosion_img.src = "../../assets/img/explosion2.png";
+    let context = this.context;
+    explosion_img.onload = function(){
+        context.drawImage(explosion_img, Enemy_posX+45, Enemey_posY, 35, 40); //PUTS THE EXPLOSION ENEMY ON SCREEN
     }
-    else if player2.score >=500{
-        winner = player2
-    }
+    let remove_explosion = setTimeout(()=>{ //SETS A TIMER FOR .5 SECONDS TO REMOVE EXPLOSION IMG
+        console.log("Timer fired");
+        let explosion_x = this.Enemies_1[index].position_x;
+        let explosion_y = this.Enemies_1[index].position_y;
+        context.clearRect(explosion_x+45,explosion_y,35,40);
+        delete this.Enemies_1[index]; //REMOVES ENEMY FROM ARRAY OF ENEMIES
+        for(let enemy in this.Enemies_1){
+            let index = this.Enemies_1.indexOf(this.Enemies_1[enemy]);
+            console.log(index+" : "+JSON.stringify(this.Enemies_1[enemy]));
+        }
+    },500)
 
-
-    */
   }
 
   public enemyPass(//player
@@ -193,8 +210,8 @@ startTimer() {
       switch(value){
         case('ArrowRight'):
             return 'right';
-        case('ArrowUp'):
-                return 'up';
+        //case('ArrowUp'):
+               // return 'up';
         case('ArrowLeft'):
                 return 'left';
         case('ArrowDown'):
@@ -234,6 +251,7 @@ startTimer() {
         let stop = false;
         let rows = 1;
         let pos_y = 5;
+        let pos_x = 50;
         for(var i= 0; i < images.length; i++){
             if(i == images.length -1 && !stop){ // WE WANT TO STOP IF 3 ROWS HAVE ALREADY BE DRAWN
                 if(rows == 3){
@@ -244,11 +262,13 @@ startTimer() {
                 pos_y += 65
             }
             let invader = new Enemy();
-            invader.position_x = 33+40 * i;
-            invader.position_y = pos_y;
+            invader.position_x = pos_x;
+            invader.position_y = pos_y+145;
             enemies.push(invader);
             var img = images[i];
             ctx.drawImage(img, 25+85 * i, pos_y, 35,40);
+            pos_x+=88;
+            //ctx.drawImage(img, 25+85 * i, pos_y, 35,40);
         }
         let space_img = document.createElement("img");
         space_img.src = "../../assets/img/spaceship.png";
@@ -261,3 +281,13 @@ startTimer() {
   }
 
 }
+
+/* TODO:
+
+    1. The x an y positions of the invaders are the same as the ones I get for the click event properties
+    2. Find out why the for loop in checkIfEnemyWasShot() is not catching the bullet that hit some enemies.
+
+*/
+
+//y+145
+//x+88
